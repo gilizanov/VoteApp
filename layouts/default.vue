5745121a707287445151ceee5ef12924ffb08788
Ruslan Gilizanov <gilizanov.r@ya.ru>
2025-07-21 18:56:24 +0400

    add default layout, sidebar, svg icons

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <SideBar />
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+}
+
+.content {
+  flex-grow: 1;
+  padding: 16px;
+}
+</style>
